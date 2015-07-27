25354d3039a9dbf9a78e41fceef668b7bbd42b39
Drew French <rectangletangle@gmail.com>
2015-07-26 21:13:59 -0700

    added port

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 
-isProd = Boolean(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL);
+isProd = Boolean(process.env.PORT);
 
-port = 5000;
+port = process.env.PORT || 5000;
 
 module.exports = {
   port: port,
 
   shortPath: '/s/',
 
-  db: { 
+  db: {
     url: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/notshort'
   },
 
   srvr: {
     domain: isProd ? 'https://notshort.herokuapp.com' : 'http://localhost:' + port
-  }  
+  }
 };
 
